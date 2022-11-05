5e42e3b9603f4f2562994e85c18919cc252e9746
timsonner <66705347+timsonner@users.noreply.github.com>
2022-11-05 01:05:43 -0600

    add model, add view stack views to image view

--- Solar System/Solar System/Model/Planet.swift
+++ Solar System/Solar System/Model/Planet.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+class Planet {
+    let name: String
+    let diameterInKMs: Double
+    let dayLengthInHours: Double
+    let millionKMsFromSun: Double
+    
+    init(name: String, diameterInKMs: Double, dayLengthInHours: Double, millionKMsFromSun: Double) {
+        self.name = name
+        self.diameterInKMs = diameterInKMs
+        self.dayLengthInHours = dayLengthInHours
+        self.millionKMsFromSun = millionKMsFromSun
+    }
+}
--- Solar System/Solar System/Model/PlanetController.swift
+++ Solar System/Solar System/Model/PlanetController.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+class PlanetController {
+    static let planets:[Planet] = [
+        Planet(name: "Mercury", diameterInKMs: 4879, dayLengthInHours: 4222.6, millionKMsFromSun: 57.9),
+        Planet(name: "Venus", diameterInKMs: 12104, dayLengthInHours: 2802, millionKMsFromSun: 108.2),
+        Planet(name: "Earth", diameterInKMs: 12756, dayLengthInHours: 24, millionKMsFromSun: 149.6),
+        Planet(name: "Mars", diameterInKMs: 6792, dayLengthInHours: 24.7, millionKMsFromSun: 227.9),
+        Planet(name: "Jupiter", diameterInKMs: 142984, dayLengthInHours: 9.9, millionKMsFromSun: 778.6),
+        Planet(name: "Saturn", diameterInKMs: 120536, dayLengthInHours: 10.7, millionKMsFromSun: 1433.5),
+        Planet(name: "Uranus", diameterInKMs: 51118, dayLengthInHours: 17.2, millionKMsFromSun: 2872.5),
+        Planet(name: "Neptune", diameterInKMs: 49528, dayLengthInHours: 1601, millionKMsFromSun: 4495.1),
+    ]
+}
